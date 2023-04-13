e1a4f3ca65f95a1093be478af9f023417451288d
redaphid <iam@aaronherres/com>
2023-04-13 15:34:35 -0700

    webgl shader does something

--- src/visualizer.js
+++ src/visualizer.js
@@ -104,6 +104,7 @@
 
     // Draw the geometry.
     gl.drawArrays(gl.TRIANGLES, 0, 6);
+    requestAnimationFrame(draw);
   }
 
   const vertexShaderSource = `#version 300 es
@@ -170,7 +171,6 @@
   gl.bindTexture(gl.TEXTURE_2D, texture);
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 255, 255]));
 
-  requestAnimationFrame(draw);
 
   const image = new Image();
   image.src  = './test_img.png';
@@ -180,4 +180,5 @@
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
     gl.generateMipmap(gl.TEXTURE_2D);
   })
+  requestAnimationFrame(draw);
 })()

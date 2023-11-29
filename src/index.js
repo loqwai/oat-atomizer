0bf3e61083157544e108178baebe4b6680557b79
redaphid <iam@aaronherres/com>
2023-11-28 19:13:53 -0700

    how does it react to audio still?

--- src/index.js
+++ src/index.js
@@ -8,12 +8,10 @@ const main = async () => {
 
 
   document.querySelector('h1').remove();
-  const audio = document.querySelector('audio');
   const audioData = new AudioData();
   await audioData.start();
 
   const canvas = document.querySelector('#visualizer');
-  const analyzer = document.querySelector('#analyzer');
 
   // const slimeMold = new SlimeMold(canvas);
   // slimeMold.start();
@@ -23,41 +21,6 @@ const main = async () => {
 
   document.onclick = () => viz.startTime = performance.now();
   document.onkeydown = () => viz.startTime = performance.now();
-
-  requestAnimationFrame(() => adjustParameters(audioData, viz));
-
-  /**
-   * @type {CanvasRenderingContext2D}
-   */
-  const ctx = analyzer.getContext('2d');
-
-  const drawLoudness = () => {
-    /** @type CanvasRenderingContext2D */
-    ctx.clearRect(0, 0, analyzer.width, analyzer.height);
-
-    audioData.loudnesses.forEach((loudness, i) => {
-      const x = i / audioData.loudnesses.length * analyzer.width;
-      const height = loudness / 256 * analyzer.height / 2;
-      ctx.fillStyle = 'white';
-      ctx.fillRect(x, analyzer.height, 1, -height);
-    })
-    requestAnimationFrame(drawLoudness);
-  }
-  requestAnimationFrame(drawLoudness);
-
-  const drawFFT = () => {
-    const waveform = audioData.getFrequencyData();
-
-
-    for (let i = 0; i < waveform.length; i++) {
-      const x = i / waveform.length * analyzer.width;
-      const height = waveform[i] / 256 * analyzer.height / 2;
-      // draw a rectangle down from the top of the canvas
-      ctx.fillRect(x, 0, analyzer.width / waveform.length, height);
-    }
-    requestAnimationFrame(drawFFT);
-  }
-  requestAnimationFrame(drawFFT);
 }
 
 
--- src/shaders/trippy1.glsl
+++ src/shaders/trippy1.glsl
@@ -1,31 +1,29 @@
 #version 300 es
 precision highp float;
 
-uniform sampler2D iChannel0;
+// uniform sampler2D iChannel0;
 uniform vec3 iResolution;
 uniform float iTime;
-uniform float RADIUS;
-uniform float SPEED;
-uniform vec2 colorScheme1;
-uniform vec2 colorScheme2;
-uniform float colorSchemeMix;
-uniform float anomaly;
 
-uniform float energy;
-uniform float spectralFlatness;
-uniform float spectralCentroid;
-uniform float spectralSpread;
-uniform float spectralSkewness;
-uniform float spectralKurtosis;
-uniform float spectralCrest;
-uniform float spectralSlope;
-uniform float spectralRolloff;
+// uniform float energy;
+// uniform float spectralFlatness;
+// uniform float spectralCentroid;
+// uniform float spectralSpread;
+// uniform float spectralSkewness;
+// uniform float spectralKurtosis;
+// uniform float spectralCrest;
+// uniform float spectralSlope;
+// uniform float spectralRolloff;
 
 out vec4 fragColor;
 
-// credit: https://www.shadertoy.com/view/ls3BDH
-// ... existing shader code ...
-// Palette function
+float smoothedEnergy = 0.0;
+float smoothedSpectralCentroid = 0.0;
+float smoothedSpectralSkewness = 0.0;
+float smoothedSpectralKurtosis = 0.0;
+float smoothedSpectralCrest = 0.0;
+float smoothedSpectralFlatness = 0.0;
+
 vec3 palette( float t ) {
     vec3 a = vec3(0.5, 0.5, 0.5);  // Base color
     vec3 b = vec3(0.5, 0.5, 0.5);  // Amplitude of the cosine waves
@@ -41,19 +39,35 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
     vec2 uv0 = uv;
     vec3 finalColor = vec3(0.0);
 
+		// smoothedEnergy = smoothstep(smoothedEnergy, energy, 0.0001);
+		// smoothedSpectralCentroid = smoothstep(smoothedSpectralCentroid, spectralCentroid, 0.1);
+		// smoothedSpectralSkewness = smoothstep(smoothedSpectralSkewness, spectralSkewness, 0.1);
+		// smoothedSpectralKurtosis = smoothstep(smoothedSpectralKurtosis, spectralKurtosis, 0.1);
+		// smoothedSpectralCrest = smoothstep(smoothedSpectralCrest, spectralCrest, 0.1);
+		// smoothedSpectralFlatness = smoothstep(smoothedSpectralFlatness, spectralFlatness, 0.1);
+
+		smoothedEnergy = 1.0;
+		smoothedSpectralCentroid = 1.0;
+		smoothedSpectralSkewness = 1.0;
+		smoothedSpectralKurtosis = 1.0;
+		smoothedSpectralCrest = 1.0;
+		smoothedSpectralFlatness = 1.0;
+
     for (float i = 0.0; i < 4.0; i++) {
-        uv = fract(uv * (1.5 + spectralFlatness * 0.5)) - 0.5; // Modify pattern based on spectral flatness
+        uv = fract(uv * (1.5 + smoothedSpectralFlatness * 0.5)) - 0.5; // Modify pattern based on spectral flatness
 
-        float d = length(uv) * exp(-length(uv0) * energy); // Use energy to control the intensity
 
-        vec3 col = palette(length(uv0) + i * .4 + iTime * .4 + spectralCentroid * 0.005); // Shift color palette with spectral centroid
 
-        d = sin(d * (8. + spectralSkewness * 0.5) + iTime) / 8.; // Modify pattern with spectral skewness
+        float d = length(uv) * exp(-length(uv0) * smoothedEnergy); // Use energy to control the intensity
+
+        vec3 col = palette(length(uv0) + i * .4 + iTime * .4 + smoothedSpectralCentroid * 0.005); // Shift color palette with spectral centroid
+
+        d = sin(d * (8. + smoothedSpectralSkewness * 0.5) + iTime) / 8.; // Modify pattern with spectral skewness
         d = abs(d);
 
-        d = pow(0.01 / d, 1.2 + spectralKurtosis * 0.01); // Use spectral kurtosis to control the sharpness
+        d = pow(0.01 / d, 1.2 + smoothedSpectralKurtosis * 0.01); // Use spectral kurtosis to control the sharpness
 
-        finalColor += col * d * (1.0 + spectralCrest * 0.1); // Modify color intensity based on spectral crest
+        finalColor += col * d * (1.0 + smoothedSpectralCrest * 0.1); // Modify color intensity based on spectral crest
     }
 
     fragColor = vec4(finalColor, 1.0);

f91681672e151cdff80835556aa77e84b729e981
redaphid <iam@aaronherres.com>
2023-11-30 16:40:12 -0700

    boy has stars go through him

--- src/shaders/boy.glsl
+++ src/shaders/boy.glsl
@@ -8,27 +8,56 @@ uniform vec3 iResolution;
 uniform float iTime;
 out vec4 fragColor;
 
+// Function to create a fuzzy circle/star
+float circle(vec2 uv, vec2 position, float radius) {
+    float len = length(uv - position);
+    return smoothstep(radius, radius - 0.01, len);
+}
+
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     // Normalized coordinates
     vec2 uv = vec2(fragCoord.x, iResolution.y - fragCoord.y) / iResolution.xy;
 
-    // Sample the audio texture
-    float audioSample = texture(iChannel0, vec2(0.0, 0.5)).r; // Sampling a specific point in the audio texture
+    // Sample the audio texture for intensity
+    float audioIntensity = texture(iChannel0, vec2(0.0, 0.5)).r;
 
-    // Create a vertical gradient affected by audio
-    vec3 topColor = vec3(audioSample, 0.0, 0.8); // Audio affects the red channel
-    vec3 bottomColor = vec3(0.4, audioSample, 0.6); // Audio affects the green channel
+    // Modify the speed of orbiting based on audio intensity
+    float orbitSpeed = 1.0 + 2.0 * audioIntensity;
+
+    // Create a vertical gradient
+    vec3 topColor = vec3(0.0, 0.0, 0.8); // Medium blue
+    vec3 bottomColor = vec3(0.4, 0.0, 0.6); // Dark purple
     vec3 gradientColor = mix(bottomColor, topColor, uv.y);
 
     // Sample the silhouette texture
     vec4 silhouetteColor = texture(iChannel1, uv);
+    // Orbiting stars
+    vec3 starColor = vec3(0.0);
+    for (int i = 0; i < 5; i++) {
+        float angle = orbitSpeed * iTime + float(i) * 0.628;
+        vec2 starPos = vec2(0.5) + 0.25 * vec2(cos(angle), sin(angle));
+        float starSize = 0.02;
+        float starIntensity = circle(uv, starPos, starSize);
 
-    // Determine if the pixel should be part of the silhouette
-    if (silhouetteColor.r < 0.01) {
-        fragColor = vec4(bottomColor, 1.0); // Render gradient unaffected by audio
-    } else {
-        fragColor = vec4(topColor, 1.0); // Render gradient affected by audio
+        // Check if the star is touching the silhouette
+        if (texture(iChannel1, starPos).r < 0.01) {
+            // Star overlaps with silhouette, make only the overlapping part dark red
+            starColor += vec3(starIntensity * (1.0 - silhouetteColor.r), 0.0, 0.0);
+            // make the silhouette transparent
+            silhouetteColor.rgb = mix(silhouetteColor.rgb, vec3(1.0, 0.0, 0.0), starIntensity);
+        } else {
+            // Star does not overlap, render normally
+            starColor += vec3(starIntensity);
+        }
     }
+
+    // Combine the gradient and stars
+    vec3 color = mix(gradientColor, starColor, step(0.01, silhouetteColor.r));
+
+    // Set the alpha channel of silhouetteColor to 1.0 to keep it opaque
+    silhouetteColor.a = 1.0;
+
+    fragColor = vec4(color, silhouetteColor.a);
 }
 
 void main(void) {

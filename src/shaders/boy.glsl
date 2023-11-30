87d3d0b13e533b51e30eb721950a5dc4540ec13d
redaphid <iam@aaronherres.com>
2023-11-30 15:37:36 -0700

    finally render a single image

--- src/shaders/boy.glsl
+++ src/shaders/boy.glsl
@@ -9,33 +9,29 @@ uniform float iTime;
 uniform int iIters;
 out vec4 fragColor;
 
-
-
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     // Normalized coordinates
-    vec2 uv = vec2(fragCoord.x, iResolution.y - fragCoord.y) / iResolution.xy;
+  vec2 uv = vec2(fragCoord.x, iResolution.y - fragCoord.y) / iResolution.xy;
 
     // Create a vertical gradient
-    vec3 topColor = vec3(0.0, 0.0, 0.8); // Medium blue
-    vec3 bottomColor = vec3(0.4, 0.0, 0.6); // Dark purple
-    vec3 gradientColor = mix(bottomColor, topColor, uv.y);
+  vec3 topColor = vec3(0.0f, 0.0f, 0.8f); // Medium blue
+  vec3 bottomColor = vec3(0.4f, 0.0f, 0.6f); // Dark purple
+  vec3 gradientColor = mix(bottomColor, topColor, uv.y);
 
     // Sample the silhouette texture
-    vec4 silhouetteColor = texture(iChannel1, uv); // Assuming the silhouette is in iChannel0
+  vec4 silhouetteColor = texture(iChannel1, uv); // Assuming the silhouette is in iChannel0
 
     // //Determine if the pixel should be part of the silhouette
-    // if (silhouetteColor.r < 0.1) { // Adjust threshold as needed
-    //     fragColor = vec4(1.0, 0.0, 0.0, 1.0); // Render silhouette
-    // } else {
-    //      fragColor = vec4(0.,1.0,0.0,1.0); // Render silhouette
-    // }
-    fragColor = silhouetteColor;
+  if(silhouetteColor.r < 0.01f) { // Adjust threshold as needed
+    fragColor = vec4(0.0f, 0.0f, 0.0f, 1.0f); // Render silhouette
+  } else {
+    fragColor = vec4(gradientColor, 1.0f); // Render gradient
+  }
 
-    // fragColor = vec4(gradientColor, 1.0); // Render gradient
 }
 
 void main(void) {
-	vec4 color = vec4(0);
-	mainImage(color, gl_FragCoord.xy);
-	fragColor = color;
+  vec4 color = vec4(0);
+  mainImage(color, gl_FragCoord.xy);
+  fragColor = color;
 }

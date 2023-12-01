532a5e3fb6243df29e8e50374dcb85b4949ded74
redaphid <iam@aaronherres.com>
2023-11-30 18:48:05 -0700

    fun intersections

--- src/shaders/boy.glsl
+++ src/shaders/boy.glsl
@@ -10,62 +10,67 @@ out vec4 fragColor;
 
 // Function to create a fuzzy circle/star
 float circle(vec2 uv, vec2 position, float radius) {
-    float len = length(uv - position);
-    return smoothstep(radius, radius - 0.01, len);
+  float len = length(uv - position);
+  return smoothstep(radius, radius - 0.01f, len);
 }
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     // Normalized coordinates
-    vec2 uv = vec2(fragCoord.x, iResolution.y - fragCoord.y) / iResolution.xy;
+  vec2 uv = vec2(fragCoord.x, iResolution.y - fragCoord.y) / iResolution.xy;
 
     // Sample the audio texture for intensity
-    float audioIntensity = texture(iChannel0, vec2(0.0, 0.5)).r;
+  float audioIntensity = texture(iChannel0, vec2(0.0f, 0.5f)).r;
 
     // Modify the speed of orbiting based on audio intensity
-    float orbitSpeed = 1.0 + 2.0 * audioIntensity;
+  float orbitSpeed = 1.0f;
 
     // Create a vertical gradient
-    vec3 topColor = vec3(0.0, 0.0, 0.8); // Medium blue
-    vec3 bottomColor = vec3(0.4, 0.0, 0.6); // Dark purple
-    vec3 gradientColor = mix(bottomColor, topColor, uv.y);
+  vec3 topColor = vec3(0.0f, 0.0f, 0.8f); // Medium blue
+  vec3 bottomColor = vec3(0.4f, 0.0f, 0.6f); // Dark purple
+
+  vec3 gradientColor = mix(bottomColor, topColor, uv.y);
+
+    // Calculate distortion radius based on star intensity and audio intensity
+  float distortionRadius = audioIntensity * 0.05f; // Adjust the factor for the desired distortion radius
+    // Offset the UV coordinates to create distortion
+  vec2 distortedUV = uv + distortionRadius * (uv - 1.5f);
 
     // Sample the silhouette texture
-    vec4 silhouetteColor = texture(iChannel1, uv);
-    // if the silhouette is close to black, make it the gradient color
-    if(silhouetteColor.g < 0.1) {
-        silhouetteColor.rgb = gradientColor;
-    } else {
-        // otherwise, make it black
-        silhouetteColor.rgb = vec3(0.0);
-    }
+  vec4 silhouetteColor = texture(iChannel1, uv);
+
+    // Check if the silhouette is close to black, make it the gradient color; otherwise, make it black
+  silhouetteColor.rgb = mix(gradientColor, vec3(0.0f), step(0.1f, silhouetteColor.g));
 
     // Orbiting stars
-    vec3 starColor = vec3(0.0);
-    for (int i = 0; i < 5; i++) {
-        float angle = orbitSpeed * iTime + float(i) * 0.628;
-        vec2 starPos = vec2(0.5) + 0.25 * vec2(cos(angle), sin(angle));
-        float starSize = 0.02;
-        float starIntensity = circle(uv, starPos, starSize);
+  vec3 starColor = vec3(0.0f);
+  for(int i = 0; i < 5; i++) {
+    float angle = orbitSpeed * iTime + float(i) * 0.628f;
+    vec2 starPos = vec2(0.5f) + 0.25f * vec2(cos(angle), sin(angle));
+    float starSize = 0.0009f * pow(1.f + audioIntensity, 10.f);
 
-        // Check if the star is touching the silhouette
-        if (texture(iChannel1, starPos).r < 0.01) {
-            // Star overlaps with silhouette, make only the overlapping part dark red
-            starColor += vec3(starIntensity * (1.0 - silhouetteColor.r), 0.0, 0.0);
-            // make the silhouette transparent
-            silhouetteColor.rgb = mix(silhouetteColor.rgb, vec3(1.0, 0.0, 0.0), starIntensity);
-        } else {
-            gradientColor = mix(gradientColor, vec3(1.0, 1.0, 1.0), starIntensity);
-        }
+    float starIntensity = circle(uv, starPos, starSize);
+
+    // Check if the star is touching the silhouette
+    float starDistance = texture(iChannel1, starPos).r;
+    if(starDistance < 0.01f) {
+      // Star overlaps with silhouette, make only the overlapping part dark red
+      // inverse gradient
+      vec3 altColor = mix(gradientColor, vec3(0.0f), step(0.1f, silhouetteColor.g));
+      starColor += mix(starColor, altColor,starIntensity/audioIntensity);
+    } else {
+      gradientColor = mix(gradientColor, vec3(1.0f, 0.1f, 0.4f), pow(starDistance, 2.) * starIntensity);
     }
-    // Combine the gradient and stars
-    vec3 color = mix(gradientColor, starColor, step(0.01, silhouetteColor.r));
+  }
 
-    fragColor = vec4(color, silhouetteColor.a);
+    // Combine the gradient and stars
+  vec3 color = mix(gradientColor, starColor, step(0.01f, silhouetteColor.r));
+
+    // Use the distorted UV coordinates for the final color
+  fragColor = vec4(color, silhouetteColor.a);
 }
 
 void main(void) {
-    vec4 color = vec4(0);
-    mainImage(color, gl_FragCoord.xy);
-    fragColor = color;
+  vec4 color = vec4(0);
+  mainImage(color, gl_FragCoord.xy);
+  fragColor = color;
 }
-

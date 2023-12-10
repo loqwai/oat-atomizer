fa810b82c068e44da64fcb1a53ba1436889bb8bf
redaphid <iam@aaronherres/com>
2023-12-10 10:12:12 -0700

    optimized stattracker. Trying to get this mobile-friendly

--- src/StatTracker.js
+++ src/StatTracker.js
@@ -1,65 +1,59 @@
-class StatTracker {
+class OptimizedStatTracker {
   constructor(historySize) {
     this.historySize = historySize;
-    this.values = [];
+    this.queue = []; // To keep the last 'historySize' values
+    this.runningSum = 0;
+    this.runningSumOfSquares = 0;
     this.mean = -1;
     this.standardDeviation = -1;
-    this.zScore = -1;
     this.min = Infinity;
     this.max = -Infinity;
   }
 
   set(value) {
     if (typeof value !== 'number') throw new Error('StatTracker can only track numbers');
-    this.values.push(value);
 
-    if (this.values.length > this.historySize) {
-      const removedValue = this.values.shift();
-      if (removedValue === this.min) {
-        // Recalculate min if the removed value was the minimum
-        this.min = Math.min(...this.values);
+    // Update min and max
+    this.min = Math.min(this.min, value);
+    this.max = Math.max(this.max, value);
+
+    // Add new value
+    this.queue.push(value);
+    this.runningSum += value;
+    this.runningSumOfSquares += value * value;
+
+    // Remove oldest value if necessary
+    if (this.queue.length > this.historySize) {
+      const removedValue = this.queue.shift();
+      this.runningSum -= removedValue;
+      this.runningSumOfSquares -= removedValue * removedValue;
+
+      // Check if we need to update min or max
+      if (removedValue === this.min || removedValue === this.max) {
+        this.recalculateMinMax();
       }
-      if (removedValue === this.max) {
-        // Recalculate max if the removed value was the maximum
-        this.max = Math.max(...this.values);
-      }
-    } else {
-      // Initialize min and max if history is not yet full
-      this.min = Math.min(this.min, value);
-      this.max = Math.max(this.max, value);
     }
 
-    this.calculateMeanAndStandardDeviation();
+    // Recalculate mean and standard deviation
+    this.mean = this.runningSum / this.queue.length;
+    const meanSquare = this.mean * this.mean;
+    const meanOfSquares = this.runningSumOfSquares / this.queue.length;
+    this.standardDeviation = Math.sqrt(meanOfSquares - meanSquare);
   }
 
   get() {
     return {
       mean: this.mean || -1,
       standardDeviation: this.standardDeviation || -1,
-      zScore: this.zScore || -1,
-      min: this.min == Infinity ? -1 : this.min,
-      max: this.max == -Infinity ? -1 : this.max,
+      min: this.min === Infinity ? -1 : this.min,
+      max: this.max === -Infinity ? -1 : this.max,
     };
   }
 
-  calculateMeanAndStandardDeviation() {
-    if (this.values.length === 0) {
-      this.mean = -1;
-      this.standardDeviation = -1;
-      this.zScore = -1;
-      return;
-    }
-
-    const sum = this.values.reduce((acc, value) => acc + value, 0);
-    this.mean = sum / this.values.length;
-
-    const squaredDifferences = this.values.map((value) => (value - this.mean) ** 2);
-    const sumSquaredDifferences = squaredDifferences.reduce((acc, value) => acc + value, 0);
-    this.standardDeviation = Math.sqrt(sumSquaredDifferences / this.values.length);
-
-    const lastValue = this.values[this.values.length - 1];
-    this.zScore = Math.abs((lastValue - this.mean) / this.standardDeviation);
+  recalculateMinMax() {
+    this.min = Math.min(...this.queue);
+    this.max = Math.max(...this.queue);
   }
 }
 
-export { StatTracker };
+export { OptimizedStatTracker as StatTracker };
